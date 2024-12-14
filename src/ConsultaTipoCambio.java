17d1bc08ea6c833d74909606b04625396d0cedfb
José <joseflorescreativo@gmail.com>
2024-12-14 14:30:41 -0500

    Se agregó un timeount de 10 segundos y un controlador de estado de la respuesta.

--- src/ConsultaTipoCambio.java
+++ src/ConsultaTipoCambio.java
@@ -3,6 +3,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -21,9 +23,16 @@ public class ConsultaTipoCambio {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(uri)
+                .header("Accept-Language", "es-ES") // Ejemplo de header
+                .timeout(Duration.ofSeconds(10)) // Timeout de 10 segundos
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Verificar el código de estado de la respuesta
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Error en la solicitud: " + response.statusCode());
+        }
 
         Gson gson = new Gson();
         return gson.fromJson(response.body(), JsonObject.class);
